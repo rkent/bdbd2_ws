1f0ef4ca233a472dc1220b31b664cd23b0ddf24a
R. Kent James <kent@caspia.com>
2022-01-20 22:39:03 +0000

    Get sound working on Respeaker

---
+++ .devcontainer/atest.sh
@@ -0,0 +1,3 @@
+#aplay --device plughw:CARD=ArrayUAC10,DEV=0 /usr/share/sounds/alsa/Front_Center.wav
+#aplay --device default:CARD=ArrayUAC10 /usr/share/sounds/alsa/Front_Center.wav
+aplay /usr/share/sounds/alsa/Front_Center.wav
